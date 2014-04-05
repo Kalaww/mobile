919416d7b26d55ef885e7d955b7f6fe9775049e8
cedeify <cedeify@hotmail.fr>
2014-04-05 14:42:38 +0200

    Amélioration de l'affichage de l'abre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+import math
 
 def main():
     fichier = open("test.txt", "r")
@@ -8,10 +9,11 @@ def main():
     print(liste)
     n = build(liste)
     print(n)
-    n.setDistance()
+    if type(n) is not Poid:
+        n.setDistance()
     
     fenetre = tkinter.Tk()
-    canvas = tkinter.Canvas(fenetre,width=400, height=400, background="white")
+    canvas = tkinter.Canvas(fenetre,width=800, height=600, background="white")
     canvas.pack()
     canvas.update()
     afficher(canvas, n)
@@ -22,10 +24,12 @@ def main():
 def build(l):
 	n = Noeud()
 	if type(l[0]) is int:
-	    print("0 est un int")
 	    n.gauche = Poid(l[0])
 	elif type(l[0]) is list:
 	    n.gauche = build(l[0])
+	if len(l) < 2:
+	    return n.gauche
+	
 	if type(l[1]) is int:
 	    n.droit = Poid(l[1])
 	elif type(l[1]) is list:
@@ -37,23 +41,42 @@ def afficher(canvas, n):
     height = canvas.winfo_height()
     canvas.delete("all")
     echelle = 0.5
-    hauteur = height*0.1
-    dessineNoeud(canvas, n, width//2, height*0.1, echelle, hauteur)
+    echellePoid = (min(width, height)*0.8//len(n))//n.maximum()
+    echelleH = height*0.8//n.profondeurMax()
+    
+    if type(n) is Poid:
+        canvas.create_line(width//2, 0, width//2, height*0.2)
+        longueur = echellePoid*n.valeur
+        canvas.create_oval(width//2 - longueur//2, height*0.2, width//2 + longueur//2, height*0.2+longueur)
+    else:
+    	canvas.create_line(width*n.valeur, 0, width*n.valeur, height*0.1)
+    	dessineNoeud(canvas, n, width//2, height*0.1, echelle, echellePoid, echelleH)
     
     canvas.update()
     
     
-def dessineNoeud(canvas, n, x, y, echelle, hauteur):
+def dessineNoeud(canvas, n, x, y, echelle, echellePoid, echelleH):
     width = canvas.winfo_width()
     height = canvas.winfo_height()
-    xS = x - width*echelle//2
-    canvas.create_line(xS, y, xS+width*echelle, y)
-    canvas.create_line(xS, y, xS, y+hauteur)
-    canvas.create_line(xS+width*echelle, y, xS+width*echelle, y+hauteur)
+    xG = x - width*echelle*n.valeur
+    xD = xG+echelle*width
+    canvas.create_line(xG, y, xD, y)
+    canvas.create_line(xG, y, xG, y+echelleH)
+    canvas.create_line(xD, y, xD, y+echelleH)
     
+    if type(n.droit) is Poid:
+        longueur = echellePoid*n.droit.valeur
+        canvas.create_oval(xD-longueur//2, y+echelleH, xD+longueur//2, y+echelleH+longueur)
+    else:
+        dessineNoeud(canvas, n.droit, xD, y+echelleH, echelle*echelle, echellePoid, echelleH)
     if type(n.gauche) is Poid:
-    
-    
+        longueur = echellePoid*n.gauche.valeur
+        canvas.create_oval(xG-longueur//2, y+echelleH, xG+longueur//2, y+echelleH+longueur)
+    else:
+        dessineNoeud(canvas, n.gauche, xG, y+echelleH, echelle*echelle, echellePoid, echelleH)
+
+
+#### NOEUD ####
 class Noeud:
     
     def __init__(self):
@@ -77,9 +100,29 @@ class Noeud:
             self.droit.setDistance()
         self.valeur = self.droit.peser() / (self.gauche.peser()+self.droit.peser())
         print("VALEUR = {}".format(+self.valeur))
+    
+    def maximum(self):
+        g = self.gauche.maximum()
+        d = self.droit.maximum()
+        if g > d:
+            return g
+        else:
+            return d
+    
+    def __len__(self):
+        return len(self.gauche) + len(self.droit)
+        
+    def profondeurMax(self):
+        g = self.gauche.profondeurMax()
+        d = self.droit.profondeurMax()
+        if g > d:
+            return g+1
+        else:
+            return d+1
 
 
-class Poid:
+#### POID ####
+class Poid(Noeud):
     
     def __init__(self, v):
         self.valeur = v
@@ -92,3 +135,12 @@ class Poid:
     
     def peser(self):
         return self.valeur
+    
+    def maximum(self):
+        return self.valeur
+    
+    def __len__(self):
+        return 1
+    
+    def profondeurMax(self):
+        return 1
